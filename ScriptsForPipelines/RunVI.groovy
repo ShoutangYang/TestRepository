e699f5490c80358cc2f8461bf2f8cad16c63b736
roxanakarami <roxana.karami@ni.com>
2017-04-06 17:01:07 -0500

    Update RunVI.groovy

--- ScriptsForPipelines/RunVI.groovy
+++ ScriptsForPipelines/RunVI.groovy
@@ -1,1 +1,21 @@
+import groovy.json.JsonOutput
 
+def call(viName) {
+
+        echo 'Running VI '+ viName
+
+        def vi_run_json = JsonOutput.toJson(['VI Name': "${vipName}"])//"LabVIEW Version":"${lvVersion}"])
+
+        echo vi_run_json       
+
+        def vip_run_response = httpRequest "http://localhost:8002/LabVIEWCIService/VIP_INSTALL?JSON="+java.net.URLEncoder.encode(vi_run_json, "UTF-8").replaceAll("\\+", "%20")
+
+        println("Status: "+ vip_run_response.status)
+
+        println("Content: "+vip_run_response.content)       
+
+        echo 'Magic wait of 5 seconds...'
+
+        sleep(5)
+
+}
