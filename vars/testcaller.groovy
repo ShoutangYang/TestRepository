67501e195c3059f412e0db929ede9e4526dc6f1c
roxanakarami <roxana.karami@ni.com>
2017-04-07 16:59:17 -0500

    Create testcaller.groovy

---
+++ vars/testcaller.groovy
@@ -0,0 +1,18 @@
+ #!/usr/bin/env groovy
+//Leave the above line alone.  It identifies this as a groovy script.
+
+
+def call(viPath)
+{
+node
+  {
+    stage ('Temp Directories')
+      {
+        bat 'mkdir build_temp'
+      }
+    stage ('Run VI'
+      {
+       RunVI(viPath)
+      }
+   }
+}
