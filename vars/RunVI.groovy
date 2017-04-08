fca40f219006ea191bb48a4ed8afb9ef86d91608
roxanakarami <roxana.karami@ni.com>
2017-04-07 23:45:55 -0500

    Update RunVI.groovy

--- vars/RunVI.groovy
+++ vars/RunVI.groovy
@@ -6,7 +6,7 @@ def call(path) {
        // echo 'Running VI '+ viName
 
         //def vi_run_json = JsonOutput.toJson(['VI Name': "${viName}"])//"LabVIEW Version":"${lvVersion}"])
-        def vi_run_json = JsonOutput.toJson(['VI_Path': "${WORKSPACE}\\${path}", Executor_Number: env.EXECUTOR_NUMBER.toString(), 'Wait_Until_Done': "false"])
+        def vi_run_json = JsonOutput.toJson([Executor_Number: env.EXECUTOR_NUMBER.toString(),'VI_Path': "${WORKSPACE}\\${path}", 'Wait_Until_Done': "false"])
         echo vi_run_json       
 
         def vi_run_response = httpRequest "http://localhost:8002/LabVIEWCIService/Run_VI?JSON="+java.net.URLEncoder.encode(vi_run_json, "UTF-8").replaceAll("\\+", "%20")
