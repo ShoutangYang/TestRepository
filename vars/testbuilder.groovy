7c4cc749559b24493f65e1b0397cd3661b38745d
roxanakarami <roxana.karami@ni.com>
2017-04-19 10:13:37 -0500

    Update testbuilder.groovy

--- vars/testbuilder.groovy
+++ vars/testbuilder.groovy
@@ -1,11 +1,12 @@
 import groovy.json.JsonOutput
 
-def call(path){
+def call(projPath, buildTarget, buildSpec)
+{
         echo 'Run basic tests before build'
         
         echo env.WORKSPACE+'\\build_temp'
         echo "${WORKSPACE}\\${path}"
-        def build_json = JsonOutput.toJson(["Executor_Number": "1", 'Workspace_Path' : env.WORKSPACE+'\\build_temp' , 'Build_Spec':"test-build", 'Target': "My Computer" , 'Project_Path': "${WORKSPACE}\\${path}" , 'Workspace_Path': env.WORKSPACE+'\\build_temp', 'Auto_Version_Increment': "true".toBoolean()])
+        def build_json = JsonOutput.toJson(["Executor_Number": env.EXECUTOR_NUMBER.toString(), 'Workspace_Path' : env.WORKSPACE+'\\build_temp' , 'Build_Spec': buildSpec, 'Target': buildTarget , 'Project_Path': "${WORKSPACE}\\${projPath}" , 'Workspace_Path': env.WORKSPACE+'\\build_temp', 'Auto_Version_Increment': "true".toBoolean()])
         echo build_json
       //  Executor_Number, Workspace_Path, Build_Spec, Target, Project_Path, Auto_Version_Increment - boolean, env.EXECUTOR_NUMBER.toString()
         
