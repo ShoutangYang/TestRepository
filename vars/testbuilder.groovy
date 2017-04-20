087c790b000dd07087713cf1e69e3dc05c10d43c
roxanakarami <roxana.karami@ni.com>
2017-04-20 12:07:32 -0500

    Update testbuilder.groovy

--- vars/testbuilder.groovy
+++ vars/testbuilder.groovy
@@ -7,7 +7,7 @@ def call(projPath, buildTarget, buildSpec)
         
         echo env.WORKSPACE+'\\build_temp'
        // echo "${WORKSPACE}\\${path}"
-        def build_json = JsonOutput.toJson(["Executor_Number": env.EXECUTOR_NUMBER.toString(), 'Workspace_Path' : env.WORKSPACE+'\\build_temp' , 'Build_Spec': buildSpec, 'Target': buildTarget , 'Project_Path': "${WORKSPACE}\\${projPath}" , 'Workspace_Path': env.WORKSPACE+'\\build_temp', 'Auto_Version_Increment': "true".toBoolean()])
+        def build_json = JsonOutput.toJson(["Executor_Number": env.EXECUTOR_NUMBER.toString(), "Workspace_Path" : env.WORKSPACE+'\\build_temp' , "Build_Spec": "${buildSpec}", 'Target': "${buildTarget}" , 'Project_Path': "${WORKSPACE}\\${projPath}" , "Workspace_Path": env.WORKSPACE+'\\build_temp', "Auto_Version_Increment": "true".toBoolean()])
         echo build_json
       //  Executor_Number, Workspace_Path, Build_Spec, Target, Project_Path, Auto_Version_Increment - boolean, env.EXECUTOR_NUMBER.toString()
         
