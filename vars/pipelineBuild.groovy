4f08dcb790dbc40e8eeff96c1fa75e78092348c5
roxanakarami <roxana.karami@ni.com>
2017-05-03 13:12:19 -0500

    Updating with echo comments

--- vars/pipelineBuild.groovy
+++ vars/pipelineBuild.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 //Leave the above line alone.  It identifies this as a groovy script.
-//Call from Jenkinsfile is testcaller(viPath, projPath, buildTarget, buildSpec)
+
 
 def call(viPath, projPath, buildTarget, buildSpec)
 {
@@ -8,7 +8,8 @@ node
   {
     stage('PreClean')
       {
-      preClean()
+       echo 'Cleaning out the workspace'
+       preClean()
       }
     stage ('SCM_Checkout'){
         echo 'Attempting to get source from repo...'
@@ -20,7 +21,7 @@ node
       }
     stage ('Run VI')
       {
-       echo 'Attempting to build the VI specified'
+       echo 'Attempting to run the VI specified'
         runVI(viPath)
       }
     stage ('LabVIEW Build')
