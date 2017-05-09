6d15cde2cb08fc3ccaa9a95a917b772e8c4b0308
roxanakarami <roxana.karami@ni.com>
2017-05-09 09:56:34 -0500

    Updating to account for valid response range

--- vars/runVI.groovy
+++ vars/runVI.groovy
@@ -10,11 +10,13 @@ def call(path) {
         //"${WORKSPACE}\\${path}"
        echo vi_run_json       
 
-        def vi_run_response = httpRequest "http://localhost:8002/LabVIEWCIService/Run_VI?JSON="+java.net.URLEncoder.encode(vi_run_json, "UTF-8").replaceAll("\\+", "%20")
+        def vi_run_response = httpRequest validResponseCodes: "200,500", url: "http://localhost:8002/LabVIEWCIService/Run_VI?JSON="+java.net.URLEncoder.encode(vi_run_json, "UTF-8").replaceAll("\\+", "%20")
 
-        println("Status: "+ vi_run_response.status)
-
-        println("Content: "+vi_run_response.content)       
+        println("Status: "+vi_run_response.status)
+        println("Content: "+vi_run_response.content)
+        if (vi_run_response.status!=200){
+                error("Call to CI Server method LabVIEW_Build failed with error: "+vi_run_response.content)
+            }
 
         echo 'Magic wait of 5 seconds...'
 
